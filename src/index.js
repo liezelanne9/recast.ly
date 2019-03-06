d524f860f33fc42e804205f6c6ac937a9fe5a6ba
Liezel Manalo <liezelanne9@gmail.com>
2019-03-05 18:42:01 -0800

    Rendered basic components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+import exampleVideoData from './data/exampleVideoData';
 
+ReactDOM.render(<App videos={exampleVideoData}/>, document.getElementById("app"));
